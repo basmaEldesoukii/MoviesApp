e0c9b043e6597db4d972a0186e4d3c7c2b69be35
Basma Eldesouky <41978905+basmaEldesouky@users.noreply.github.com>
2024-03-21 16:00:01 +0200

    remove unneeded files

--- movie_details/src/main/java/com/banquemisr/movie_details/domain/entity/Genre.kt
+++
@@ -1,6 +0,0 @@
-package com.banquemisr.movie_details.domain.entity
-
-data class Genre(
-    val id: Int,
-    val name: String
-)
--- movie_details/src/main/java/com/banquemisr/movie_details/domain/entity/ProductionCountry.kt
+++
@@ -1,6 +0,0 @@
-package com.banquemisr.movie_details.domain.entity
-
-data class ProductionCountry(
-    val iso_3166_1: String,
-    val name: String
-)
